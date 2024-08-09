03a7b580facc2792011bd95fb4d84b494e5f85a6
Ohm0025 <toyohm0025@gmail.com>
2024-08-09 18:29:23 +0700

    config:env2

--- db/pool.js
+++ db/pool.js
@@ -6,7 +6,7 @@ dotenv.config({ path: "config.env" });
 const pool = new Pool({
   database: process.env.DATABASE,
   host: process.env.HOST,
-  port: process.env.PORT,
+  port: process.env.PORTDB,
   user: process.env.USER,
   password: process.env.PASSWORD,
 });
